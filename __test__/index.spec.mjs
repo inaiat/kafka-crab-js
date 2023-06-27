fa0567aa232d71177d4206d16955d256e82b685d
Inaiat Moraes <inaiat@gmail.com>
2023-06-27 00:05:06 -0300

    fix unit tests

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,8 +1,5 @@
 import test from 'ava'
 import { KafkaClient } from '../index.js'
-import timersPromises from 'node:timers/promises'
-import { Buffer } from 'node:buffer'
-import { nanoid } from 'nanoid'
 
 const broker = "localhost:29092"
 const brokerId = "kafka-crab-client-id"
