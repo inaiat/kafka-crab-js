dace36277a29c1628775f63bccff25686b3a7f2a
Inaiat Moraes <inaiat@gmail.com>
2024-11-25 12:10:55 -0300

    fix: reexports

---
+++ js-src/kafka-client.ts
@@ -0,0 +1,49 @@
+import { ConsumerConfiguration, KafkaClientConfig, KafkaConfiguration, ProducerConfiguration } from './js-binding.js'
+
+import { KafkaStreamReadable } from './kafka-stream-readable'
+
+/**
+ * KafkaClient class
+ */
+export class KafkaClient {
+  private readonly kafkaClientConfig: KafkaClientConfig
+  /**
+   * Creates a KafkaClient instance
+   * @throws {Error} If the configuration is invalid
+   */
+  constructor(private readonly kafkaConfiguration: KafkaConfiguration) {
+    this.kafkaClientConfig = new KafkaClientConfig(kafkaConfiguration)
+  }
+
+  /**
+   * Creates a KafkaProducer instance
+   * @param {ProducerConfiguration} [producerConfiguration] - Optional producer configuration
+   * @returns {KafkaProducer} A KafkaProducer instance
+   */
+  createProducer(producerConfiguration: ProducerConfiguration) {
+    if (producerConfiguration) {
+      return this.kafkaClientConfig.createProducer(producerConfiguration)
+    }
+    return this.kafkaClientConfig.createProducer({})
+  }
+
+  /**
+   * Creates a KafkaConsumer instance
+   * @param {ConsumerConfiguration} consumerConfiguration - Consumer configuration
+   * @returns {KafkaConsumer} A KafkaConsumer instance
+   * @throws {Error} If the configuration is invalid
+   */
+  createConsumer(consumerConfiguration: ConsumerConfiguration) {
+    return this.kafkaClientConfig.createConsumer(consumerConfiguration)
+  }
+
+  /**
+   * Creates a KafkaStreamReadable instance
+   * @param {ConsumerConfiguration} consumerConfiguration - Consumer configuration
+   * @returns {KafkaStreamReadable} A KafkaStreamReadable instance
+   * @throws {Error} If the configuration is invalid
+   */
+  createStreamConsumer(consumerConfiguration: ConsumerConfiguration) {
+    return new KafkaStreamReadable(this.kafkaClientConfig.createConsumer(consumerConfiguration))
+  }
+}
