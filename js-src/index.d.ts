bb776e99cab8dad41c1d9ad48c0f441efd93757e
Inaiat Moraes <inaiat@gmail.com>
2024-11-25 12:14:13 -0300

    chore: remove generated files from js-src

--- js-src/index.d.ts
+++
@@ -1,4 +0,0 @@
-export type { ConsumerConfiguration, KafkaConfiguration, KafkaCrabError, Message, MessageProducer, OffsetModel, PartitionOffset, ProducerConfiguration, ProducerRecord, RecordMetadata, RetryStrategy, TopicPartitionConfig, } from './js-binding';
-export { CommitMode, KafkaClientConfig, KafkaConsumer, KafkaProducer, PartitionPosition, SecurityProtocol, } from './js-binding';
-export { KafkaClient } from './kafka-client';
-export { KafkaStreamReadable } from './kafka-stream-readable';
